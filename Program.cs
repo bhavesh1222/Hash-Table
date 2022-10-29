c11c01a31c76de6c89e8e6dea75dcbbbb0fb693b
Bhavesh Bhadane Patil <113571629+bhavesh1222@users.noreply.github.com>
2022-10-29 09:00:23 +0530

    hash table

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] array = paragraph.Split(" ");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(array.Length);
+            int key = 0;
+            foreach (string word in array)
+            {
+                hash.Add(key, word);
+                key++;
+            }
+            frequency count = new frequency();
+            count.Remove(hash, "avoidable");
+        }
+    }
+}
+
+
